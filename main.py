88e865baba4b681f930210943075a2da8d038334
sourav0208 <sourav@tum.de>
2026-03-23 12:35:07 +0100

    Add triangle area computation for mesh elements

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from mesh import generate_elements, generate_nodes
+from mesh import generate_elements, generate_nodes, triangle_area
 from plots import plot_nodes, plot_mesh
 
 
@@ -12,6 +12,37 @@ def main():
     print("Elements shape:", elements.shape)
     print("First 5 nodes:\n", nodes[:5])
     print("First 5 elements:\n", elements[:5])
+
+    k = 0
+    coords = nodes[elements[k]]
+
+    print("Element index:", k)
+    print("Node indices of elements:", elements[k])
+    print("Coordinates of elements:\n", coords)
+
+    area = triangle_area(coords)
+    print("Area of elements:", area)
+
+    k = 1
+    coords = nodes[elements[k]]
+
+    print("Element index:", k)
+    print("Node indices of elements:", elements[k])
+    print("Coordinates of elements:\n", coords)
+
+    area = triangle_area(coords)
+    print("Area of elements:", area)
+
+    k = 2
+    coords = nodes[elements[k]]
+
+    print("Element index:", k)
+    print("Node indices of elements:", elements[k])
+    print("Coordinates of elements:\n", coords)
+
+    area = triangle_area(coords)
+    print("Area of elements:", area)
+
 
     plot_nodes(nodes)
     plot_mesh(nodes, elements)
--- mesh.py
+++ mesh.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def generate_nodes(nx: int, ny: int, lx: float = 1.0, ly: float = 1.0) -> np.ndarray:
+def generate_nodes(nx: int, ny: int, lx: float = 2.0, ly: float = 1.0) -> np.ndarray:
     x = np.linspace(0.0, lx, nx)
     y = np.linspace(0.0, ly, ny)
 
@@ -31,4 +31,15 @@ def generate_elements(nx: int, ny: int) -> np.ndarray:
             elements.append([n0, n3, n2])
 
     return np.array(elements, dtype=int)
+
+def triangle_area(coords):
+    x1,y1 = coords[0]
+    x2,y2 = coords[1]
+    x3,y3 = coords[2]
+
+    area = 0.5 * abs((x2-x1)*(y3-y1) - (x3-x1)*(y2-y1))
+
+    return area
+
+
 
